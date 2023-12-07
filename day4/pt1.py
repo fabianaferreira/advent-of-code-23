e9339d7c66fe5f49e698ea7f8e9a5ccf8d6287d2
Fabiana Ferreira Fonseca <fffonseca@pm.me>
2023-12-07 16:16:52 -0300

    Refactor pt1

--- day4/pt1.py
+++ day4/pt1.py
@@ -1,10 +1,10 @@
 from pathlib import Path
 import sys
-import re
 
 path_root = Path(__file__).parents[1]
 sys.path.append(str(path_root))
 
+from util import get_card_lists
 from utils.sort import merge_sort
 from utils.search import binary_search
 
@@ -12,19 +12,6 @@ from utils.search import binary_search
 # Loop over the numbers we have
 # Use binary search to find the numbers that are in both
 # O((N + M)logM)
-
-
-# Get all indexes in a line that is a symbol
-def get_card_id_and_lists(line):
-    (id, winners_str, my_numbers_str) = re.findall(r"(\d):(.*)\|(.*)", line)[0]
-    winners_list = winners_str.strip().split()
-    my_numbers_list = my_numbers_str.strip().split()
-
-    # Converting to int before returning the values
-    winners_list = [int(x) for x in winners_list]
-    my_numbers_list = [int(x) for x in my_numbers_list]
-
-    return id, winners_list, my_numbers_list
 
 
 def get_card_points(winners, my_numbers):
@@ -40,7 +27,7 @@ with open("input.txt", "r") as f:
     lines = f.readlines()
     sum = 0
     for index, line in enumerate(lines):
-        id, winners_list, my_numbers_list = get_card_id_and_lists(line)
+        winners_list, my_numbers_list = get_card_lists(line)
         merge_sort(my_numbers_list)
         sum += get_card_points(winners_list, my_numbers_list)
 
