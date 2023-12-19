15ba43817af81efc6f15eaf5906fd6f946ad81c9
Fabiana Ferreira Fonseca <fffonseca@pm.me>
2023-12-19 17:03:02 -0300

    Refact pt1 day6

--- day6/pt1.py
+++ day6/pt1.py
@@ -1,25 +1,9 @@
 import re
-import math
-
+from util import get_quantity_of_solutions, solve_equation
 
 def get_numbers_from_line(line):
     numbers = re.findall(r"(\d+)", line)
     return [int(x) for x in numbers]
-
-
-def solve_equation(distance, time):
-    delta = time**2 - 4 * distance
-    limit1 = (-time + delta**0.5) / 2
-    limit2 = (-time - delta**0.5) / 2
-
-    return math.ceil(limit1), math.floor(limit2)
-
-
-def get_quantity_of_solutions(limit1, limit2):
-    if limit1 == limit2:
-        return 1
-    return limit2 - limit1 + 1
-
 
 with open("input.txt", "r") as f:
     lines = f.readlines()
