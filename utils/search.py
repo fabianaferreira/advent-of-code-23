873c7fbb3f155b56ce4f09f22bb1987135e63fec
Fabiana Ferreira Fonseca <fffonseca@pm.me>
2023-12-22 10:02:51 -0300

    Add BFS function to utils

--- utils/search.py
+++ utils/search.py
@@ -1,4 +1,7 @@
 # Returns index of x in arr if present, else -1
+from collections import deque
+
+
 def binary_search(arr, low, high, x):
     # Check base case
     if high >= low:
@@ -19,3 +22,19 @@ def binary_search(arr, low, high, x):
     else:
         # Element is not present in the array
         return -1
+
+
+def bfs(graph, start):
+    search_queue = [(start, 0)]
+    visited = set()
+    max_distance = 0
+
+    while len(search_queue) > 0:
+        current_node, distance = search_queue.pop(0)
+        visited.add(current_node)
+        max_distance = max(max_distance, distance)
+
+        for next in graph[current_node] - visited:
+            search_queue.append((next, distance + 1))
+
+    return max_distance
